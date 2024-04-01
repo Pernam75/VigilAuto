c8f19265fa2abafec365b7c1558bfe7213038b1b
Jules Rubin <jules.rubin.jr@gmail.com>
2024-04-01 18:25:24 +0200

    added the tests for stt component

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from VigilAuto.config.configuration import ConfigurationManager
 from VigilAuto.components.llm import LLM
+from VigilAuto.components.stt import STT
 from VigilAuto import logger
 
 def test_llm():
@@ -10,3 +11,12 @@ def test_llm():
     print(api_response)
     assert api_response is not None
     assert type(api_response.content) == str
+
+def test_stt():
+    config_manager = ConfigurationManager()
+    stt_config = config_manager.get_stt_config()
+    stt = STT(config=stt_config)
+    transcription = stt.transcribe("test_audio.mp3")
+    assert transcription is not None
+    assert type(transcription) == str
+    assert transcription.lower().__contains__("test")
