b44c0632a98209698454f6d15f7ae8f47a496978
Jules Rubin <jules.rubin@efrei.net>
2024-04-06 17:18:19 +0200

    removed legacy code

--- src/VigilAuto/components/tts.py
+++ src/VigilAuto/components/tts.py
@@ -29,28 +29,3 @@ class TTS:
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
         return output_path
-
-""" LEGACY CODE
-
-from VigilAuto import logger
-from VigilAuto.entity.config_entity import TTSConfig
-from pathlib import Path
-from TTS.api import TTS as TTS_API
-
-class TTS:
-    def __init__(self, config: TTSConfig):
-        self.config = config
-        logger.info(f"Loading TTS model: {config.model_name}")
-        self.model = TTS_API(model_name=config.model_name, progress_bar=False)
-        logger.info(f"Model loaded.")
-        if not Path(config.output_folder).exists():
-            logger.info(f"Creating output folder: {config.output_folder}")
-            Path(config.output_folder).mkdir(parents=True, exist_ok=True)
-            
-
-    def synthetize(self, text_input: str, file_id: str) -> str:
-        output_path = Path(self.config.output_folder) / f"{file_id}_{self.config.file_path}"
-        logger.info(f"Synthetizing text to audio: {input}")
-        self.model.tts_to_file(text=text_input, file_path=output_path)
-        return output_path
-"""
