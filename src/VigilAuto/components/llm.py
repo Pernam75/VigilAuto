ce6002d34a77136c4d96ccd992c8269ddf89b8ea
Jules Rubin <jules.rubin@efrei.net>
2024-04-06 12:41:00 +0200

    feat(llm) added history handle with langhcain conversation buffer memory

--- src/VigilAuto/components/llm.py
+++ src/VigilAuto/components/llm.py
@@ -2,7 +2,9 @@ from VigilAuto import logger
 from VigilAuto.entity.config_entity import LLMConfig
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
-
+from langchain.prompts.prompt import PromptTemplate
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
 class LLM:
     def __init__(self, config: LLMConfig):
         self.config = config
@@ -13,3 +15,25 @@ class LLM:
         chain = prompt | self.chat
         result = chain.invoke({"text": human_prompt})
         return result
+
+class LLMChat:
+    def __init__(self, config: LLMConfig):
+        template = """
+        Tu es un assistant vocal de conduite. Grâce à une caméra intégrée dans la voiture, le taux d'alcoolémie du conducteur a été mesuré.
+        Sache qu'un verre standard d'alccol équivault à 0.2g/L. Si le taux est supérieur à 0.5g/L, tu dois conseiller au conducteur de ne pas conduire. 
+        Reste concis très concis dans tes réponses.
+        Conversation actuelle:
+        {history}
+        Utilisateur: {input}
+        Assistant Vocal:"""
+        self.config = config
+        self.llm = ChatGroq(groq_api_key=self.config.groq_api_key, model=self.config.model_name)
+        self.conversation = ConversationChain(
+            prompt=PromptTemplate(input_variables=["history", "input"], template=template),
+            llm=self.llm,
+            verbose=False,
+            memory=ConversationBufferMemory(ai_prefix="Assistant Vocal", human_prefix="Conducteur"),
+        )
+
+    def predict(self, input: str):
+        return self.conversation.predict(input=input)
